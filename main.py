964d0e00be5e2e3038170df1ee6afc519d7fa938
ilmir sharifullin <ilmirsharifullin2000@gmail.com>
2022-03-05 08:48:29 +0300

    qwe

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from werkzeug.utils import redirect
 
 from data import db_session
-from flask import Flask, render_template
-from data.users import User
+from flask import Flask, render_template, request, make_response
 
+from data.departments import Department
+from data.users import User
 from data.jobs import Jobs
 from forms.user import RegisterForm
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
@@ -45,6 +55,22 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
 
 
 def main():
